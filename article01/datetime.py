2437be3571a46688444c26c7db7f579f269a66d3
kishimotoul <6168121+kishimotoul@users.noreply.github.com>
2018-09-13 18:52:57 +0900

    pathとmethodによるアクセス制限を追加

--- article01/datetime.py
+++ article01/datetime.py
@@ -1,19 +1,30 @@
 # -*- coding:utf-8 -*-
-
 import wsgiref.simple_server
 import datetime
 
 PORT = 8000
+ALLOWED_METHOD_LIST = ['GET', 'POST']
+ALLOWED_PATH = '/utc'
 
-def api_clock(environ, start_response):
-    status  = "200 OK"
-    headers = [
-        ("Content-type", "text/plain; charset=utf-8"),
-    ]
+def api(environ, start_response):
+    method  = environ['REQUEST_METHOD']
+    path    = environ['PATH_INFO']
+    headers = [('Content-Type', 'text/plain; charset=utf-8')]
+    body    = ''
+    if method not in ALLOWED_METHOD_LIST:
+        status  = '400 Bad Request'
+    elif path != ALLOWED_PATH:
+        status  = '404 Not Found'
+    else:
+        status  = '200 OK'
+        body    = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
     start_response(status, headers)
-    return [datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")]
+    return [body]
+    
+def main():
+    httpd = wsgiref.simple_server.make_server('', PORT, api)
+    print("Micro Service on port %s" % PORT)
+    httpd.serve_forever()
 
-server = wsgiref.simple_server.make_server("", PORT, api_clock)
-print("Started on port %d" % PORT)
-
-server.serve_forever()
+if __name__ == '__main__':
+    main()
